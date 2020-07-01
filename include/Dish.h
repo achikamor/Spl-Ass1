1c27e8214aad6ece64043d2d4f30e970f3fadf56
achikamor <achikamor@gmail.com>
2020-07-02 00:09:05 +0300

    Dish.h

---
+++ include/Dish.h
@@ -0,0 +1,27 @@
+#ifndef SPL_ASS1_DISH_H
+#define SPL_ASS1_DISH_H
+
+#include <string>
+
+enum DishType{
+    VEG, SPC, BVG, ALC
+};
+
+class Dish{
+public:
+    Dish(int d_id, std::string d_name, int d_price, DishType d_type);
+    int getId() const;
+    std::string getName() const;
+    int getPrice() const;
+    DishType getType() const;
+    std::string tostring();
+    virtual ~Dish();
+private:
+    const int id;
+    const std::string name;
+    const int price;
+    const DishType type;
+};
+
+
+#endif
